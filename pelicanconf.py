be1694cb9b6779d87bf83667eb054c75ca5ae53a
Simon Hayward <simon@fastmail.to>
2017-06-26 23:04:49 +0100

    Update links

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,13 +21,10 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Python.org', 'http://python.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'https://twitter.com/simhay'),)
 
 DEFAULT_PAGINATION = 10
 
